2b813e97742577a7d478e0f7b791fbdb30ea180b
jorgeglezca98 <jorgeglezca98@gmail.com>
2018-07-02 21:21:29 +0100

    I tried to make a splash screen but I could not solve a problem with the resources for now.

---
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/activities/SplashActiviy.java
@@ -0,0 +1,32 @@
+package com.example.jorgegonzalezcabrera.outgoing.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jorgegonzalezcabrera.outgoing.R;
+import com.example.jorgegonzalezcabrera.outgoing.models.appConfiguration;
+
+import io.realm.Realm;
+
+public class SplashActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Realm database = Realm.getDefaultInstance();
+        final Intent intent;
+
+        database.beginTransaction();
+        if (database.where(appConfiguration.class).findAll() == null)
+            intent = new Intent(SplashActiviy.this, initialConfigurationActivity.class);
+        else
+            intent = new Intent(SplashActiviy.this, MainActivity.class);
+        database.commitTransaction();
+
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/applications/myApplication.java
@@ -0,0 +1,23 @@
+package com.example.jorgegonzalezcabrera.outgoing.applications;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class myApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        getConfiguration();
+    }
+
+    private void getConfiguration(){
+        RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
+
+//TODO: explore configuration possibilities
