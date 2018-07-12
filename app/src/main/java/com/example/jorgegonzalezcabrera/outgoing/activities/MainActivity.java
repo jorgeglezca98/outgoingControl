8743aed5d8171451a210e9117f52dd5697f9f7d3
jorgeglezca98 <jorgeglezca98@gmail.com>
2018-07-12 14:21:03 +0100

    Now the main activity has a viewpager to access another fragment where the user's actions will be in next updates.

--- app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/activities/MainActivity.java
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/activities/MainActivity.java
@@ -1,45 +1,30 @@
 package com.example.jorgegonzalezcabrera.outgoing.activities;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import com.example.jorgegonzalezcabrera.outgoing.R;
-import com.example.jorgegonzalezcabrera.outgoing.adapters.surplusMoneyTableAdapter;
-import com.example.jorgegonzalezcabrera.outgoing.dialogs.dialogs;
-import com.example.jorgegonzalezcabrera.outgoing.models.appConfiguration;
-import com.example.jorgegonzalezcabrera.outgoing.models.entry;
+import com.example.jorgegonzalezcabrera.outgoing.adapters.mainPagerAdapter;
+import com.example.jorgegonzalezcabrera.outgoing.fragments.actionsFragment;
+import com.example.jorgegonzalezcabrera.outgoing.fragments.mainFragment;
 
 import java.util.Vector;
 
-import io.realm.Realm;
-
-import static com.example.jorgegonzalezcabrera.outgoing.dialogs.dialogs.newEntryDialog;
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends FragmentActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    appConfiguration currentConfiguration;
-    Realm database;
-    TextView textViewCurrentMoney;
-    TextView textViewOutgoingsOfTheMonth;
-    TextView textViewIncomesOfTheMonth;
-    RecyclerView recyclerViewSurplusMoney;
-    double totalOutgoings;
-    double totalIncomes;
-    Vector<surplusMoneyTableAdapter.surplusMoneyByCategory> surplusMoneyByCategoryVector;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,31 +32,33 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
-        bindUI();
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Main"));
+        tabLayout.addTab(tabLayout.newTab().setText("Actions"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        viewPager = findViewById(R.id.viewPager);
+        Vector<Fragment> fragments = new Vector<>();
+        fragments.add(new mainFragment());
+        fragments.add(new actionsFragment());
+        viewPager.setAdapter(new mainPagerAdapter(getSupportFragmentManager(),fragments));
+        //TODO: this method is deprecated so I have to look for another option
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                newEntryDialog(MainActivity.this, currentConfiguration.getOutgoingCategoriesCategories(),
-                        currentConfiguration.getIncomeCategories(), new dialogs.OnNewEntryAccepted() {
-                            @Override
-                            public void OnClick(final String subcategory, final int type, final double value, final String description) {
-                                database.executeTransaction(new Realm.Transaction() {
-                                    @Override
-                                    public void execute(Realm realm) {
-                                        final entry newEntry = new entry(value, type, subcategory, description);
-                                        if (type == entry.type.OUTGOING.ordinal()) {
-                                            updateAfterOutgoing(value,subcategory);
-                                        } else {
-                                            updateAfterIncome(value);
-                                        }
-                                        database.copyToRealmOrUpdate(currentConfiguration);
-                                        database.copyToRealm(newEntry);
-                                    }
-                                });
-                            }
-                        });
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
@@ -83,60 +70,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    public void bindUI(){
-        database = Realm.getDefaultInstance();
-        database.executeTransaction(new Realm.Transaction() {
-            @Override
-            public void execute(@NonNull Realm realm) {
-                currentConfiguration = database.where(appConfiguration.class).findFirst();
-            }
-        });
-
-        textViewCurrentMoney = findViewById(R.id.textViewCurrentMoney);
-        textViewCurrentMoney.setText(String.format("%.2f", currentConfiguration.getCurrentMoney()));
-
-        surplusMoneyByCategoryVector = new Vector<>();
-        totalOutgoings = 0;
-        double aux;
-        textViewOutgoingsOfTheMonth = findViewById(R.id.textViewOutgoingsOfTheMonth);
-        for (int i = 0; i < currentConfiguration.getOutgoingCategoriesCategories().size(); i++) {
-            aux = 0;
-            for (int j = 0; j < currentConfiguration.getOutgoingCategoriesCategories().get(i).getSubcategories().size(); j++) {
-                aux += database.where(entry.class).equalTo("category", currentConfiguration.getOutgoingCategoriesCategories().get(i).getSubcategories().get(j).getName()).sum("valor").doubleValue();
-            }
-            totalOutgoings += aux;
-            surplusMoneyByCategoryVector.add(new surplusMoneyTableAdapter.surplusMoneyByCategory(currentConfiguration.getOutgoingCategoriesCategories().get(i), currentConfiguration.getOutgoingCategoriesCategories().get(i).getMaximum() - aux));
-
-        }
-        textViewOutgoingsOfTheMonth.setText(String.format("%.2f", totalOutgoings));
-
-        totalIncomes = 0;
-        textViewIncomesOfTheMonth = findViewById(R.id.textViewIncomeOfTheMonth);
-        for (int i = 0; i < currentConfiguration.getIncomeCategories().size(); i++) {
-            totalIncomes += database.where(entry.class).equalTo("category", currentConfiguration.getIncomeCategories().get(i).getName()).sum("valor").doubleValue();
-        }
-        textViewIncomesOfTheMonth.setText(String.format("%.2f", totalIncomes));
-
-        recyclerViewSurplusMoney = findViewById(R.id.recyclerViewSurplusMoney);
-        recyclerViewSurplusMoney.setAdapter(new surplusMoneyTableAdapter(surplusMoneyByCategoryVector));
-        recyclerViewSurplusMoney.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    public void updateAfterOutgoing(double value, String subcategory){
-        currentConfiguration.setCurrentMoney(currentConfiguration.getCurrentMoney() - value);
-        textViewCurrentMoney.setText(String.format("%.2f", currentConfiguration.getCurrentMoney()));
-        totalOutgoings += value;
-        textViewOutgoingsOfTheMonth.setText(String.format("%.2f", totalOutgoings));
-        ((surplusMoneyTableAdapter) recyclerViewSurplusMoney.getAdapter()).updateData(subcategory, value);
-    }
-
-    public void updateAfterIncome(double value){
-        currentConfiguration.setCurrentMoney(currentConfiguration.getCurrentMoney() + value);
-        textViewCurrentMoney.setText(String.format("%.2f", currentConfiguration.getCurrentMoney()));
-        totalIncomes += value;
-        textViewIncomesOfTheMonth.setText(String.format("%.2f", totalIncomes));
     }
 
     @Override
---
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/fragments/mainFragment.java
@@ -0,0 +1,132 @@
+package com.example.jorgegonzalezcabrera.outgoing.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.jorgegonzalezcabrera.outgoing.R;
+import com.example.jorgegonzalezcabrera.outgoing.activities.MainActivity;
+import com.example.jorgegonzalezcabrera.outgoing.adapters.surplusMoneyTableAdapter;
+import com.example.jorgegonzalezcabrera.outgoing.dialogs.dialogs;
+import com.example.jorgegonzalezcabrera.outgoing.models.appConfiguration;
+import com.example.jorgegonzalezcabrera.outgoing.models.entry;
+
+import java.util.Vector;
+
+import io.realm.Realm;
+
+import static com.example.jorgegonzalezcabrera.outgoing.dialogs.dialogs.newEntryDialog;
+
+public class mainFragment extends Fragment {
+
+    appConfiguration currentConfiguration;
+    Realm database;
+    TextView textViewCurrentMoney;
+    TextView textViewOutgoingsOfTheMonth;
+    TextView textViewIncomesOfTheMonth;
+    RecyclerView recyclerViewSurplusMoney;
+    double totalOutgoings;
+    double totalIncomes;
+    Vector<surplusMoneyTableAdapter.surplusMoneyByCategory> surplusMoneyByCategoryVector;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment,container,false);
+
+        bindUI(view);
+
+        return view;
+    }
+
+    public void bindUI(View view){
+        database = Realm.getDefaultInstance();
+        database.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(@NonNull Realm realm) {
+                currentConfiguration = database.where(appConfiguration.class).findFirst();
+            }
+        });
+
+        textViewCurrentMoney = view.findViewById(R.id.textViewCurrentMoney);
+        textViewCurrentMoney.setText(String.format("%.2f", currentConfiguration.getCurrentMoney()));
+
+        surplusMoneyByCategoryVector = new Vector<>();
+        totalOutgoings = 0;
+        double aux;
+        textViewOutgoingsOfTheMonth = view.findViewById(R.id.textViewOutgoingsOfTheMonth);
+        for (int i = 0; i < currentConfiguration.getOutgoingCategoriesCategories().size(); i++) {
+            aux = 0;
+            for (int j = 0; j < currentConfiguration.getOutgoingCategoriesCategories().get(i).getSubcategories().size(); j++) {
+                aux += database.where(entry.class).equalTo("category", currentConfiguration.getOutgoingCategoriesCategories().get(i).getSubcategories().get(j).getName()).sum("valor").doubleValue();
+            }
+            totalOutgoings += aux;
+            surplusMoneyByCategoryVector.add(new surplusMoneyTableAdapter.surplusMoneyByCategory(currentConfiguration.getOutgoingCategoriesCategories().get(i), currentConfiguration.getOutgoingCategoriesCategories().get(i).getMaximum() - aux));
+
+        }
+        textViewOutgoingsOfTheMonth.setText(String.format("%.2f", totalOutgoings));
+
+        totalIncomes = 0;
+        textViewIncomesOfTheMonth = view.findViewById(R.id.textViewIncomeOfTheMonth);
+        for (int i = 0; i < currentConfiguration.getIncomeCategories().size(); i++) {
+            totalIncomes += database.where(entry.class).equalTo("category", currentConfiguration.getIncomeCategories().get(i).getName()).sum("valor").doubleValue();
+        }
+        textViewIncomesOfTheMonth.setText(String.format("%.2f", totalIncomes));
+
+        recyclerViewSurplusMoney = view.findViewById(R.id.recyclerViewSurplusMoney);
+        recyclerViewSurplusMoney.setAdapter(new surplusMoneyTableAdapter(surplusMoneyByCategoryVector));
+        recyclerViewSurplusMoney.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newEntryDialog(getContext(), currentConfiguration.getOutgoingCategoriesCategories(),
+                        currentConfiguration.getIncomeCategories(), new dialogs.OnNewEntryAccepted() {
+                            @Override
+                            public void OnClick(final String subcategory, final int type, final double value, final String description) {
+                                database.executeTransaction(new Realm.Transaction() {
+                                    @Override
+                                    public void execute(Realm realm) {
+                                        final entry newEntry = new entry(value, type, subcategory, description);
+                                        if (type == entry.type.OUTGOING.ordinal()) {
+                                            updateAfterOutgoing(value,subcategory);
+                                        } else {
+                                            updateAfterIncome(value);
+                                        }
+                                        database.copyToRealmOrUpdate(currentConfiguration);
+                                        database.copyToRealm(newEntry);
+                                    }
+                                });
+                            }
+                        });
+            }
+        });
+    }
+
+    public void updateAfterOutgoing(double value, String subcategory){
+        currentConfiguration.setCurrentMoney(currentConfiguration.getCurrentMoney() - value);
+        textViewCurrentMoney.setText(String.format("%.2f", currentConfiguration.getCurrentMoney()));
+        totalOutgoings += value;
+        textViewOutgoingsOfTheMonth.setText(String.format("%.2f", totalOutgoings));
+        ((surplusMoneyTableAdapter) recyclerViewSurplusMoney.getAdapter()).updateData(subcategory, value);
+    }
+
+    public void updateAfterIncome(double value){
+        currentConfiguration.setCurrentMoney(currentConfiguration.getCurrentMoney() + value);
+        textViewCurrentMoney.setText(String.format("%.2f", currentConfiguration.getCurrentMoney()));
+        totalIncomes += value;
+        textViewIncomesOfTheMonth.setText(String.format("%.2f", totalIncomes));
+    }
+}
