0613db5647aab7c362910b30f676dd6af3909952
jorgeglezca98 <jorgeglezca98@gmail.com>
2018-07-29 13:20:39 +0100

    Unused files removed, code improvements and TODOs resolved.

--- app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/applications/myApplication.java
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/applications/myApplication.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
-public class myApplication extends Application{
+public class myApplication extends Application {
 
     public static AtomicInteger periodicEntryId;
 
@@ -17,21 +17,11 @@ public class myApplication extends Application{
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
-        getConfiguration();
-
-        if(Realm.getDefaultInstance().where(periodicEntry.class).findAll().size()!=0){
-            periodicEntryId = new AtomicInteger(Realm.getDefaultInstance().where(periodicEntry.class).max("id").intValue());
-        } else{
-            periodicEntryId = new AtomicInteger();
-            periodicEntryId.set(0);
-        }
-        //TODO: this condition can result in the malfunction of the app, it is necessary to avoid id repetitions after the removal of perodicEntry
-    }
-
-    private void getConfiguration(){
         RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
         Realm.setDefaultConfiguration(config);
+
+        Number higherId = Realm.getDefaultInstance().where(periodicEntry.class).max("id");
+        periodicEntryId = new AtomicInteger((higherId == null) ? 0 : higherId.intValue());
     }
 }
-
 //TODO: explore configuration possibilities
