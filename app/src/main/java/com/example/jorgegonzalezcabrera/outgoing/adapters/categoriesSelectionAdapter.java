41e52c4ed1092d99b69768b16fb1e0f54dfff02b
jorgeglezca98 <jorgeglezca98@gmail.com>
2018-08-14 17:35:02 +0100

    Some files that were missing in the last commit were added.

---
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/adapters/categoriesSelectionAdapter.java
@@ -0,0 +1,57 @@
+package com.example.jorgegonzalezcabrera.outgoing.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import com.example.jorgegonzalezcabrera.outgoing.R;
+import com.example.jorgegonzalezcabrera.outgoing.utilities.localUtils;
+
+import java.util.List;
+
+public class categoriesSelectionAdapter extends RecyclerView.Adapter<categoriesSelectionAdapter.ViewHolder> {
+
+    private int layout;
+    private List<String> categories;
+
+    public categoriesSelectionAdapter() {
+        layout = R.layout.category_selection_item;
+        categories = localUtils.getCategories();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(layout, viewGroup, false);
+        return new categoriesSelectionAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.bind(categories.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public CheckBox checkboxCategory;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            checkboxCategory = itemView.findViewById(R.id.checkboxCategory);
+        }
+
+        void bind(String categoryName) {
+            checkboxCategory.setChecked(true);
+            checkboxCategory.setText(categoryName);
+        }
+    }
+}
