365d3db42306d522a2733fd48cc1e913a7118ceb
jorgeglezca98 <jorgeglezca98>
2018-12-12 23:12:55 +0000

    Aesthetics changes (titles).

--- app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/fragments/fourthPageInitialConfiguration.java
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/fragments/fourthPageInitialConfiguration.java
@@ -9,8 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import com.example.jorgegonzalezcabrera.outgoing.R;
 import com.example.jorgegonzalezcabrera.outgoing.adapters.newMoneyControllersAdapter;
@@ -36,15 +34,6 @@ public class fourthPageInitialConfiguration extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fourth_fragment_initial_configuration, container, false);
-
-        final TextView informationTextView = view.findViewById(R.id.moneyControllerDescription);
-        ImageButton informationButton = view.findViewById(R.id.buttonInformation);
-        informationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                informationTextView.setVisibility(informationTextView.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
-            }
-        });
 
         RecyclerView moneyControllerRecyclerView = view.findViewById(R.id.recyclerViewMoneyControllersRequest);
         moneyControllerRecyclerView.setAdapter(moneyControllerAdapter);
--- app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/fragments/secondPageInitialConfiguration.java
+++ app/src/main/java/com/example/jorgegonzalezcabrera/outgoing/fragments/secondPageInitialConfiguration.java
@@ -10,9 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ScrollView;
-import android.widget.TextView;
 
 import com.example.jorgegonzalezcabrera.outgoing.R;
 import com.example.jorgegonzalezcabrera.outgoing.adapters.erasableItemsAdapter;
@@ -72,15 +70,6 @@ public class secondPageInitialConfiguration extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.second_fragment_initial_configuration, container, false);
-
-        final TextView informationTextView = view.findViewById(R.id.outgoingsCategoriesDescription);
-        ImageButton informationButton = view.findViewById(R.id.buttonInformation);
-        informationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                informationTextView.setVisibility(informationTextView.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
-            }
-        });
 
         recyclerViewOutgoingCategories = view.findViewById(R.id.recyclerViewOutgoingsCategoriesRequest);
         recyclerViewOutgoingCategories.setAdapter(outgoingCategoriesAdapter);
